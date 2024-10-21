c2f3d4f775f6a6844f2bb04c5ceacff7a2f08041
qweasd22 <koltsov-main@mail.ru>
2024-10-21 10:16:52 +0300

    добавил пару кнопок

--- main.py
+++ main.py
@@ -23,13 +23,18 @@ class MainWindow(QtWidgets.QMainWindow):
         load_local_doc_button = QtWidgets.QPushButton("Загрузить документ с компьютера")
         load_remote_doc_button = QtWidgets.QPushButton("Загрузить документ по ссылке")
         convert_button = QtWidgets.QPushButton("Конвертировать")
+        save_button = QtWidgets.QPushButton("Сохранить в файл")
+        cancel_button = QtWidgets.QPushButton("Закончить конвертацию")
         layout.addWidget(load_local_doc_button)
         layout.addWidget(load_remote_doc_button)
         layout.addWidget(convert_button)
-
+        layout.addWidget(save_button)
+        layout.addWidget(cancel_button)
         load_local_doc_button.clicked.connect(self.load_local_document)
         load_remote_doc_button.clicked.connect(self.load_remote_document)
         convert_button.clicked.connect(self.convert_text_to_speech)
+        save_button.clicked.connect(self.save_text_to_file)
+        cancel_button.clicked.connect(self.cancel_conversion)
         
 
         self.setCentralWidget(central_widget)
@@ -61,12 +66,22 @@ class MainWindow(QtWidgets.QMainWindow):
             print(e)
             QtWidgets.QMessageBox.warning(self, "Error", "Failed to load document.")
 
-    def convert_text_to_speech(self):
+    def convert_text_to_speech(self,):
         engine = pyttsx3.init()
         text = self.text_input.text()
         engine.say(text)
         engine.runAndWait()
 
+    def cancel_conversion(self):
+        engine = pyttsx3.init()
+        engine.stop()
+
+    def save_text_to_file(self, text):
+        engeine = pyttsx3.init()
+        text = self.text_input.text()
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Directory')
+        engeine.save_to_file(text, f'{directory}/audio.mp3')
+        engeine.runAndWait()
     
 
 if __name__ == '__main__':
